260f74d2de148f38f2aed66ccd907259c99b9aca
eycha <silvlexonst@gmail.com>
2023-05-21 20:29:05 +0900

    List

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">menu</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="mt-4">
+    <h5>블로그 메인</h5>
+    <p>하이</p>
+  </div>
+
+  <List></List>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+  components: {
+    List: List,
+  },
 };
+
+import List from "./components/List.vue";
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.mt-4 {
+  position: relative;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>블로그 글 제목</h4>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h4>블로그 글 제목</h4>
+    <p>블로그 글 날짜</p>
+  </div>
+</template>
+
+<script>
+export default {
+  //eslint-disable-next-line
+};
+</script>
+
+<style></style>
